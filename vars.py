31d9a0aedf4ed01207d092212206e04c8161565d
Mads Kristiansen <mads@plikki.com>
2024-05-20 11:14:12 +0200

    feat: split variables into different files

---
+++ vars.py
@@ -0,0 +1,20 @@
+#
+#                       ,,    ,,
+#   .g8""8q.     mm     db  `7MM              `7MMF'   `7MF'
+# .dP'    `YM.   MM           MM                `MA     ,V
+# dM'      `MM mmMMmm `7MM    MM  .gP"Ya         VM:   ,V ,6"Yb.  `7Mb,od8 ,pP"Ybd
+# MM        MM   MM     MM    MM ,M'   Yb         MM.  M'8)   MM    MM' "' 8I   `"
+# MM.      ,MP   MM     MM    MM 8M""""""         `MM A'  ,pm9MM    MM     `YMMMa.
+# `Mb.    ,dP'   MM     MM    MM YM.    ,          :MM;  8M   MM    MM     L.   I8
+#   `"bmmd"'     `Mbmo.JMML..JMML.`Mbmmd'           VF   `Moo9^Yo..JMML.   M9mmmP'
+#       MMb
+#        `bood'
+# I found out that referenceing vars from config and then referenceing other
+# referenceing config gives a error. (Makes sense) so instead we put vars here
+
+
+mod = "mod4"
+terminal = "kitty"
+keyboard_Lang = "dk"
+theme = "kanagawa"
+font = "IosevkaTerm NFM"
