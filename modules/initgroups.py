9f6f92e4df12c32cdae7cb080497e653445ed88d
Mads Kristiansen <mads@plikki.com>
2024-05-20 18:17:03 +0200

    feat: add groups to seperate file

---
+++ modules/initgroups.py
@@ -0,0 +1,54 @@
+from libqtile.config import Group, ScratchPad, DropDown
+
+# Create labels for groups and assign them a default layout.
+groups = []
+
+group_names = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "minus", "equal"]
+
+group_labels = ["", "", "", "", "", "", "", "", "", "", "", ""]
+
+group_layouts = [
+    "monadtall",
+    "monadtall",
+    "monadtall",
+    "monadtall",
+    "monadwide",
+    "monadtall",
+    "monadtall",
+    "monadtall",
+    "monadtall",
+    "monadtall",
+    "monadtall",
+    "monadtall",
+    "monadtall",
+    "monadtall",
+]
+
+# Add group names, labels, and default layouts to the groups object.
+for i in range(len(group_names)):
+    groups.append(
+        Group(
+            name=group_names[i],
+            layout=group_layouts[i].lower(),
+            label=group_labels[i],
+        )
+    )
+
+
+groups.append(
+    ScratchPad(
+        "scratchpad",
+        [
+            DropDown("term", "kitty", width=0.8, height=0.8, x=0.1, y=0.1, opacity=1),
+            DropDown(
+                "volume",
+                "pavucontrol",
+                width=0.8,
+                height=0.8,
+                x=0.1,
+                y=0.1,
+                opacity=0.8,
+            ),
+        ],
+    )
+)
