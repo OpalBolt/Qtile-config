e34abcd6f8f10b758785437a81a0a9bd9b10bc27
Mads Kristiansen <mads@plikki.com>
2024-06-15 10:55:49 +0200

    feat: Add autostart script

---
+++ modules/inithooks.py
@@ -0,0 +1,16 @@
+from libqtile import hook, qtile
+
+import subprocess
+import os
+
+
+# Startup applications
+@hook.subscribe.startup_once
+def autostart():
+    if qtile.core.name == "x11":
+        autostartscript = "~/dotfiles/qtile/scripts/x11-autostart.sh"
+    elif qtile.core.name == "wayland":
+        autostartscript = "~/dotfiles/qtile/scripts/wayland-autostart.sh"
+
+    home = os.path.expanduser(autostartscript)
+    subprocess.Popen([home])
