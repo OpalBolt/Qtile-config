f9665133f7701d07ce48bf15b49357b587e1808f
Mads Kristiansen <mads@plikki.com>
2024-06-16 11:27:12 +0200

    fix: new paths

--- modules/inithooks.py
+++ modules/inithooks.py
@@ -8,9 +8,9 @@ import os
 @hook.subscribe.startup_once
 def autostart():
     if qtile.core.name == "x11":
-        autostartscript = "~/dotfiles/qtile/scripts/x11-autostart.sh"
+        autostartscript = "~/.config/qtile/scripts/x11-autostart.sh"
     elif qtile.core.name == "wayland":
-        autostartscript = "~/dotfiles/qtile/scripts/wayland-autostart.sh"
+        autostartscript = "~/.config/qtile/scripts/wayland-autostart.sh"
 
     home = os.path.expanduser(autostartscript)
     subprocess.Popen([home])
