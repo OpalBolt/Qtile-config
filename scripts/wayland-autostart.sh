3a110c23f55de7c1beb06a28d4ab558c3a0f60c5
Mads Kristiansen <mads@plikki.com>
2024-06-16 16:59:38 +0200

    fix: include dunst config file location

--- scripts/wayland-autostart.sh
+++ scripts/wayland-autostart.sh
@@ -2,4 +2,4 @@ dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 swayidle -w timeout 600 'swaylock' &
-dunst
+dunst --conf "~/.config/dunst/dunstrc"
