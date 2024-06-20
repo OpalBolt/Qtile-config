0234480c0bf9afbbc43bb4d31b0d6e08c357afa3
Mads Kristiansen <mads@plikki.com>
2024-06-20 08:23:27 +0200

    fix: add missing currentdesktop variable

--- scripts/wayland-autostart.sh
+++ scripts/wayland-autostart.sh
@@ -1,5 +1,4 @@
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
-
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 swayidle -w timeout 600 'swaylock' &
 dunst --conf "~/.config/dunst/dunstrc"
