70f0ccc1ede63d107059896004f2a97fd165b2ed
Mads Kristiansen <mads@plikki.com>
2024-06-23 21:44:12 +0200

    feat: add missing env var for dunst to work

--- scripts/wayland-autostart.sh
+++ scripts/wayland-autostart.sh
@@ -1,4 +1,5 @@
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots &
+systemctl --user import-environment DISPLAY &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 swayidle -w timeout 600 'swaylock' &
 dunst --conf "~/.config/dunst/dunstrc"
