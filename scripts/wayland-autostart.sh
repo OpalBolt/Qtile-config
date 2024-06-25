4e8b106bec001938212ca36cc5163e5aeb32e922
Mads Kristiansen <mads@plikki.com>
2024-06-25 06:28:09 +0200

    feat: add restart to apps to ensure notifications work

--- scripts/wayland-autostart.sh
+++ scripts/wayland-autostart.sh
@@ -1,5 +1,6 @@
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots &
 systemctl --user import-environment DISPLAY &
+systemctl --user restart pipewire wireplumber.service xdg-desktop-portal xdg-desktop-portal-wlr &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 swayidle -w timeout 600 'swaylock' &
 dunst --conf "~/.config/dunst/dunstrc"
