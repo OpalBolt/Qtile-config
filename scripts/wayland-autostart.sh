4b3c0a9104ab35110d07bf1c28a748488e325296
Mads Kristiansen <mads@plikki.com>
2024-06-28 09:18:34 +0200

    fix: remake update script

--- scripts/wayland-autostart.sh
+++ scripts/wayland-autostart.sh
@@ -1,6 +1,26 @@
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots &
-systemctl --user import-environment DISPLAY &
-systemctl --user restart pipewire wireplumber.service xdg-desktop-portal xdg-desktop-portal-wlr &
+#!/bin/bash
+
+dbus-update-activation-environment --systemd \
+	WAYLAND_DISPLAY XDG_CURRENT_DESKTOP="$XDG_CURRENT_DESKTOP"
+
+# Authentication dialog
+
+pkill -f /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-swayidle -w timeout 600 'swaylock' &
-dunst --conf "~/.config/dunst/dunstrc"
+
+pkill -u "${USER}" -x pipewire\|wireplumber 1>/dev/null 2>&1
+dbus-run-session pipewire &>/dev/null &
+
+# Start xdg-desktop-portal-wlr
+
+pkill -f /usr/libexec/xdg-desktop-portal-wlr
+systemctl --user start xdg-desktop-portal-wlr
+
+pkill -f dunst
+dunst &
+
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+swayidle \
+	timeout 5 'qtile cmd-obj -o core -f hide_cursor' resume 'qtile cmd-obj -o core -f unhide_cursor' \
+	timeout 300 'swaylock' \  &
+#	timeout 600 'wlopm --off \*' resume 'wlopm --on \*' &
