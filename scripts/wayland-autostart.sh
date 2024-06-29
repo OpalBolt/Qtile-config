fedcf80a8a7afd8e667f27857011b85d82a1323d
Mads Kristiansen <mads@plikki.com>
2024-06-29 23:44:59 +0200

    fix: Update startup script

--- scripts/wayland-autostart.sh
+++ scripts/wayland-autostart.sh
@@ -5,22 +5,21 @@ dbus-update-activation-environment --systemd \
 
 # Authentication dialog
 
-pkill -f /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+pkill -f /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
-pkill -u "${USER}" -x pipewire\|wireplumber 1>/dev/null 2>&1
+pkill -u "${USER}" -x pipewire\|wireplumber 1>/dev/null 2>&1 &
 dbus-run-session pipewire &>/dev/null &
 
 # Start xdg-desktop-portal-wlr
 
-pkill -f /usr/libexec/xdg-desktop-portal-wlr
-systemctl --user start xdg-desktop-portal-wlr
+pkill -f /usr/libexec/xdg-desktop-portal-wlr &
+systemctl --user start xdg-desktop-portal-wlr &
 
-pkill -f dunst
+pkill -f dunst &
 dunst &
 
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-swayidle \
-	timeout 5 'qtile cmd-obj -o core -f hide_cursor' resume 'qtile cmd-obj -o core -f unhide_cursor' \
-	timeout 300 'swaylock' \  &
+# swayidle \
+# 	timeout 5 'qtile cmd-obj -o core -f hide_cursor' resume 'qtile cmd-obj -o core -f unhide_cursor' \
+# 	timeout 300 'swaylock'
 #	timeout 600 'wlopm --off \*' resume 'wlopm --on \*' &
